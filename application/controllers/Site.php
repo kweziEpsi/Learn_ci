28a35e53f89321735f9953dd2c80a1817149791e
kweziEpsi <kwezi@digilims.com>
2021-10-08 12:03:21 +0200

    Using model in controller with autoloader

--- application/controllers/Site.php
+++ application/controllers/Site.php
@@ -2,10 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Site extends CI_Controller {
-	public function __construct() {
-		parent::__construct();
-		$this->load->model('Site_model');
-	}
 	public function index()
 	{
 		$this->load->view('include/header');
--- application/models/Site_model.php
+++ application/models/Site_model.php
@@ -3,6 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Site_model extends CI_Model {
     function run_query(){
-        return "Thank you for your visit";
+        return "Thank you for your visit!!";
     }
 }
