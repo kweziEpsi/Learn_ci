dfb8f44856d4269a00b20a2f33d2536a238546cf
kweziEpsi <kwezi@digilims.com>
2021-10-12 10:16:48 +0200

    Added comments to controller and model

--- application/controllers/Item.php
+++ application/controllers/Item.php
@@ -11,71 +11,129 @@ class Item extends CI_Controller {
         $this->load->library('session');
      }
 
+    /**
+    * Start list view function.
+    *
+   */
     public function index()
     {
-        $data['data'] = $this->Item_model->get_item();
+        $data['data'] = $this->Item_model->get_item(); //link to get items function from model
  
         $this->load->view('include/header');       
-        $this->load->view('item/index',$data);
+        $this->load->view('item/index',$data); //display view with list of items
         $this->load->view('include/footer');
     }
+    /**
+    * End list view function.
+    *
+   */
 
+    /**
+    * Start create view function.
+    *
+   */
     public function create()
    {
       $this->load->view('include/header');
       $this->load->view('item/create');
       $this->load->view('include/footer');   
    }
+   /**
+    * End create view function.
+    *
+   */
 
+   /**
+    * Start save to db function.
+    *
+   */
    public function store(){
-        $this->form_validation->set_rules('title', 'Title', 'required');
-        $this->form_validation->set_rules('description', 'Description', 'required');
+        $this->form_validation->set_rules('title', 'Title', 'required'); //validate title
+        $this->form_validation->set_rules('description', 'Description', 'required'); //validate description
 
 
-        if ($this->form_validation->run() == FALSE){
-            $this->session->set_flashdata('errors', validation_errors());
-            redirect(base_url('item/create'));
+        if ($this->form_validation->run() == FALSE){ //check if validation rules are false
+            $this->session->set_flashdata('errors', validation_errors()); //error message
+            redirect(base_url('item/create')); //redirect to create view 
         }else{
-        $this->Item_model->save_item();
-        redirect(base_url('item/list'));
+        $this->Item_model->save_item(); //link to save item function in model
+        redirect(base_url('item/list')); //redirect to list view
         }
    }
+   /**
+    * End save to db function.
+    *
+   */
 
+   /**
+    * Start display single item function.
+    *
+   */
    public function show($id){
-    $item = $this->Item_model->find_item($id);
+    $item = $this->Item_model->find_item($id); //link to find item function in model
 
     $this->load->view('include/header');
-    $this->load->view('item/show',array('item'=>$item));
+    $this->load->view('item/show',array('item'=>$item)); //display single item on view
     $this->load->view('include/footer');
    }
+   /**
+    * End display single item function.
+    *
+   */
 
+   /**
+    * Start edit Function.
+    *
+   */
    public function edit($id)
    {
-       $item = $this->Item_model->find_item($id);
+       $item = $this->Item_model->find_item($id); //Find item
 
        $this->load->view('include/header');
-       $this->load->view('item/edit',array('item'=>$item));
+       $this->load->view('item/edit',array('item'=>$item)); //Display item to edit
        $this->load->view('include/footer');
    }
+   /**
+    * End edit Function.
+    *
+   */
 
+
+   /**
+    * Start Update Function.
+    *
+   */
    public function update($id)
    {
-        $this->form_validation->set_rules('title', 'Title', 'required');
-        $this->form_validation->set_rules('description', 'Description', 'required');
+        $this->form_validation->set_rules('title', 'Title', 'required'); //validate title
+        $this->form_validation->set_rules('description', 'Description', 'required'); //validate description
 
 
-        if ($this->form_validation->run() == FALSE){
-            $this->session->set_flashdata('errors', validation_errors());
-            redirect(base_url('item/edit/'.$id));
+        if ($this->form_validation->run() == FALSE){ //check if validation rules are false
+            $this->session->set_flashdata('errors', validation_errors()); //error message
+            redirect(base_url('item/edit/'.$id)); //redirect to edit view
         }else{ 
-          $this->Item_model->update_item($id);
-          redirect(base_url('item'));
+          $this->Item_model->update_item($id); //link to update item function from model 
+          redirect(base_url('item')); //redirect to list
         }
    }
+   /**
+    * End Update Function.
+    *
+   */
 
+
+   /**
+    * Start Delete Function.
+    *
+   */
    public function delete($id)
    {
-       $item = $this->Item_model->delete_item($id);
-       redirect(base_url('item'));
+       $item = $this->Item_model->delete_item($id); //link to delete item function from model
+       redirect(base_url('item')); //redirect to list
    }
+   /**
+    * End Delete Data Function.
+    *
+   */
 }
--- application/models/Item_model.php
+++ application/models/Item_model.php
@@ -2,46 +2,85 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Item_model extends CI_Model {
-
+    /**
+    * Start get items function.
+    *
+   */
     public function get_item(){
-        if(!empty($this->input->get("search"))){
-          $this->db->like('title', $this->input->get("search"));
-          $this->db->or_like('description', $this->input->get("search")); 
+        if(!empty($this->input->get("search"))){ //check if input fields are not empty
+          $this->db->like('title', $this->input->get("search")); //get the title
+          $this->db->or_like('description', $this->input->get("search")); //get the description 
         }
-        $query = $this->db->get("tbl_items");
-        return $query->result();
+        $query = $this->db->get("tbl_items"); //get items db table
+        return $query->result(); //return results
     }
+    /**
+    * End get items function.
+    *
+   */
 
+   /**
+    * Start save items function.
+    *
+   */
     public function save_item()
     {    
         $data = array(
-            'title' => $this->input->post('title'),
-            'description' => $this->input->post('description')
+            'title' => $this->input->post('title'), //get title from input data
+            'description' => $this->input->post('description') //get description from input data
         );
-        return $this->db->insert('tbl_items', $data);
+        return $this->db->insert('tbl_items', $data); //insert into items table and return data
     }
+    /**
+    * End save items function.
+    *
+   */
 
+   /**
+    * Start find item function.
+    *
+   */
     public function find_item($id){
-      return $this->db->get_where('tbl_items', array('id' => $id))->row();
+      return $this->db->get_where('tbl_items', array('id' => $id))->row(); //return data from items table where 'id' = $id
     }
+    /**
+    * End find item function.
+    *
+   */
 
+   /**
+    * Start update item function.
+    *
+   */
     public function update_item($id) 
     {
         $data=array(
-            'title' => $this->input->post('title'),
-            'description'=> $this->input->post('description')
+            'title' => $this->input->post('title'), //get the title data input
+            'description'=> $this->input->post('description') //get the description data input
         );
-        if($id==0){
-            return $this->db->insert('tbl_items',$data);
+        if($id==0){ //check if $id == 0
+            return $this->db->insert('tbl_items',$data); //insert data to items table and return data
         }else{
-            $this->db->where('id',$id);
-            return $this->db->update('tbl_items',$data);
+            $this->db->where('id',$id); //find data from items table where 'id' = $id
+            return $this->db->update('tbl_items',$data); //update and return data 
         }  
     }
+    /**
+    * End update item function.
+    *
+   */
     
+   /**
+    * Start delete item function.
+    *
+   */
     public function delete_item($id)
     {
-        return $this->db->delete('tbl_items', array('id' => $id));
+        return $this->db->delete('tbl_items', array('id' => $id)); //find data from items table where 'id' = $id and delete item
     }
+    /**
+    * End delete item function.
+    *
+   */
     
 }
