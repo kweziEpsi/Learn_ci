a2968aae14892af57e65c551db7bb5069699db3c
kweziEpsi <kwezi@digilims.com>
2021-10-22 10:34:40 +0200

    Index edit

--- application/views/item/index.php
+++ application/views/item/index.php
@@ -13,25 +13,26 @@
 <table class="table table-bordered table-hover">
   <thead>
       <tr>
-          <th>Title</th>
-          <th>Description</th>
-          <th width="220px">Action</th>
+        <th></th>
+        <th>Title</th>
+        <th>Description</th>
+        <th width="220px">Action</th>
       </tr>
   </thead>
   <tbody>
    <?php foreach ($data as $item) { ?>      
       <tr>
-          <td><?php echo $item->title; ?></td>
-          <td><?php echo $item->description; ?></td>          
-      <td>
-            <a class="btn btn-info" href="<?php echo base_url('item/read-more/'.$item->id) ?>"><i class='fa fa-eye' style='font-size:24px'></i></a>
+        <td width=auto;><?php echo $item->id; ?></td>
+        <td width=auto;><?php echo $item->title; ?></td>
+        <td><?php echo $item->description; ?></td>          
+        <td>
+            <center><a class="btn btn-info" href="<?php echo base_url('item/read-more/'.$item->id) ?>"><i class='fa fa-eye' style='font-size:24px'></i></a>
             <a class="btn btn-primary" href="<?php echo base_url('item/edit/'.$item->id) ?>"><i class='fas fa-edit' style='font-size:24px'></i></a>
             <?php 
                 if (isset($_SESSION['user_name'])) { ?>
-                    <a class="btn btn-danger" href="<?php echo base_url('item/delete/'.$item->id) ?>"><i class='fas fa-trash' style='font-size:24px'></i></a>
-            <?php } ?>
-            
-      </td>     
+                    <a class="btn btn-danger" href="<?php echo base_url('item/delete/'.$item->id) ?>"><i class='fas fa-trash' style='font-size:24px'></i></a></center>
+            <?php } ?>    
+        </td>     
       </tr>
       <?php } ?>
   </tbody>
